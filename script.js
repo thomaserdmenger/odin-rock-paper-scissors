60c2d866b5c63e01cb3230365d59b22f0683f685
Thomas Erdmenger <thomas.erdmenger@icloud.com>
2023-07-09 17:23:58 +0200

    Console.log result when click on button

--- script.js
+++ script.js
@@ -5,74 +5,92 @@ const getComputerChoice = () => {
   return arr[randomNum]
 }
 
+// getting players choices (buttons)
+const btnRock = document.querySelector('#rock')
+const btnPaper = document.querySelector('#paper')
+const btnScissors = document.querySelector('#scissors')
+
+// listen for click events on buttons and call playround
+btnRock.addEventListener('click', playRound)
+btnPaper.addEventListener('click', playRound)
+btnScissors.addEventListener('click', playRound)
+
 // getting the winner of a single round
-const playRound = (playerSelection, computerSelection) => {
-  const playersChoice = playerSelection.toLowerCase()
-  const computersChoice = computerSelection.toLowerCase()
+function playRound(e) {
+  const playersChoice = e.target.id
+  console.log(playersChoice)
+  const computersChoice = getComputerChoice()
   const arr = ['rock', 'paper', 'scissors']
 
   // player wins
   if (playersChoice === arr[0] && computersChoice === arr[2]) {
-    return `You win: ${playersChoice} beats ${computersChoice}`
+    console.log(`You win: ${playersChoice} beats ${computersChoice}`)
   } else if (playersChoice === arr[1] && computersChoice === arr[0]) {
-    return `You win: ${playersChoice} beats ${computersChoice}`
+    console.log(`You win: ${playersChoice} beats ${computersChoice}`)
   } else if (playersChoice === arr[2] && computersChoice === arr[1]) {
-    return `You win: ${playersChoice} beats ${computersChoice}`
+    console.log(`You win: ${playersChoice} beats ${computersChoice}`)
   }
 
   // player loses
   if (playersChoice === arr[2] && computersChoice === arr[0]) {
-    return `You loose: ${computersChoice} beats ${playersChoice}`
+    console.log(`You loose: ${computersChoice} beats ${playersChoice}`)
   } else if (playersChoice === arr[0] && computersChoice === arr[1]) {
-    return `You loose: ${computersChoice} beats ${playersChoice}`
+    console.log(`You loose: ${computersChoice} beats ${playersChoice}`)
   } else if (playersChoice === arr[1] && computersChoice === arr[2]) {
-    return `You loose: ${computersChoice} beats ${playersChoice}`
+    console.log(`You loose: ${computersChoice} beats ${playersChoice}`)
   }
 
   // tie between player and computerd
-  else {
-    return `It's a tie: you both choose ${computersChoice} and ${playersChoice}`
+  if (playersChoice === arr[0] && computersChoice === arr[0]) {
+    console.log(`It's a tie: you both choose ${computersChoice} and ${playersChoice}`)
+  } else if (playersChoice === arr[1] && computersChoice === arr[1]) {
+    console.log(`It's a tie: you both choose ${computersChoice} and ${playersChoice}`)
+  } else if (playersChoice === arr[2] && computersChoice === arr[2]) {
+    console.log(`It's a tie: you both choose ${computersChoice} and ${playersChoice}`)
   }
 }
+
+// function handleClick(e) {
+//   console.log(e.target.id)
+// }
 
 // getting the winner of the game within 5 rounds
-const game = () => {
-  let playersScore = 0
-  let computersScore = 0
-  let tie = 0
-  let rounds = 0
+// const game = () => {
+//   let playersScore = 0
+//   let computersScore = 0
+//   let tie = 0
+//   let rounds = 0
 
-  // getting the winner with 5 points
+//   // getting the winner with 5 points
+//   while (playersScore < 5 && computersScore < 5) {
+//     let result = playRound(prompt('Choose between rock, paper or scissors'), getComputerChoice())
 
-  while (playersScore < 5 && computersScore < 5) {
-    let result = playRound(prompt('Choose between rock, paper or scissors'), getComputerChoice())
+//     if (result.includes('win')) {
+//       playersScore += 1
+//       rounds += 1
+//     } else if (result.includes('loose')) {
+//       computersScore += 1
+//       rounds += 1
+//     } else {
+//       tie += 1
+//       rounds += 1
+//     }
+//   }
 
-    if (result.includes('win')) {
-      playersScore += 1
-      rounds += 1
-    } else if (result.includes('loose')) {
-      computersScore += 1
-      rounds += 1
-    } else {
-      tie += 1
-      rounds += 1
-    }
-  }
-
-  // find the winner
-  if (playersScore > computersScore) {
-    return `🥳 You win: ${playersScore} vs. ${computersScore} points. (${tie} ties in ${rounds} rounds).`
-  } else if (playersScore < computersScore) {
-    return `😢 The computer wins: ${computersScore} vs. ${playersScore} points. (${tie} ties in ${rounds} rounds).`
-  } else {
-    return `🔁 It's a tie: ${playersScore} vs. ${computersScore}. (${tie} ties in ${rounds} rounds). Try one more game. `
-  }
-}
+//   // find the winner
+//   if (playersScore > computersScore) {
+//     return `🥳 You win: ${playersScore} vs. ${computersScore} points. (${tie} ties in ${rounds} rounds).`
+//   } else if (playersScore < computersScore) {
+//     return `😢 The computer wins: ${computersScore} vs. ${playersScore} points. (${tie} ties in ${rounds} rounds).`
+//   } else {
+//     return `🔁 It's a tie: ${playersScore} vs. ${computersScore}. (${tie} ties in ${rounds} rounds). Try one more game. `
+//   }
+// }
 
 // Create a message in the browser
-const container = document.querySelector('.message-container')
-const message = document.createElement('span')
-message.textContent = game()
-message.classList.add('style')
+// const container = document.querySelector('.message-container')
+// const message = document.createElement('span')
+// message.textContent = game()
+// message.classList.add('style')
 
-container.insertAdjacentElement('afterbegin', message)
+// container.insertAdjacentElement('afterbegin', message)
